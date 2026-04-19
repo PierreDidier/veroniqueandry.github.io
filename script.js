65db0e0c1032ea2b206bbb48e362f1c158a6b0bc
Pierre_Didier <pierre.didier.contact@gmail.com>
2026-04-19 14:31:06 +0200

    update pictures

--- script.js
+++ script.js
@@ -6,7 +6,10 @@ const views = {
     'nav-apropos': document.getElementById('view-about'),
     'link-feux-follets': document.getElementById('view-feux-follets'),
     'link-untitled': document.getElementById('view-untitled'),
-    'link-saisons': document.getElementById('view-saisons')
+    'link-saisons': document.getElementById('view-saisons'),
+    'link-verger': document.getElementById('view-verger'),
+    'link-verticales': document.getElementById('view-verticales')
+
 };
 
 function showView(viewId) {
